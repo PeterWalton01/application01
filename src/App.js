758ccba0843933497f6932442b051cb3464aa7b4
PeterW <heaving01@gmail.com>
2022-06-02 00:34:23 +0100

    Changed the main file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Amended by Peter W.
+        </p>
+        <p>
+          A further change to make the deployment more meaningful.
         </p>
         <a
           className="App-link"
